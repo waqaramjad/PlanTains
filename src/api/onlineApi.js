36e3542ea726604be85d2cf3af40d237cb6a30a9
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-02-17 16:16:23 +0800

    api: add movie comment

--- src/api/onlineApi.js
+++ src/api/onlineApi.js
@@ -1,6 +1,29 @@
 import firebase from 'firebase'
 
+//
+// movies api
+//
+
 export function getMovieByID(_id) {
   return firebase.database().ref('Movies/' + _id).once('value')
   .then(data => data.val())
+}
+
+export function getCommentsByMovieID(_id, cb) {
+	const commentRef = firebase.database().ref('comment/' + _id)
+
+	if (typeof cb === 'function') {
+	  // bind changes for live updating
+	  commentRef.on('child_added', cb)
+	}
+
+  return commentRef.once('value').then(data => data.val())
+}
+
+export function postComment (_id, c, n) {
+	console.log(_id, c, n)
+	const commentRef = firebase.database().ref('comment/' + _id)
+  const d = (new Date()).getTime()
+
+  return commentRef.push({c, n, d})
 }
