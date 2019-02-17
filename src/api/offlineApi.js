af40b45358e9e6eca3f2fce342bb030069aea9bd
Jay Lord P. Torres <isens.jaylord@gmail.com>
2019-02-17 11:04:39 +0800

    api: test

---
+++ src/api/offlineApi.js
@@ -0,0 +1,9 @@
+
+
+export function getMovieByID(_id) {
+	return {
+		Title: 'Jay lord Movie',
+		discription: 'Jay lord Movie Jay lord Movie Jay lord Movie Jay lord Movie. Jay lord Movie .Jay lord Movie .Jay lord Movie.',
+		TrailerUrl: ''
+	}
+}
